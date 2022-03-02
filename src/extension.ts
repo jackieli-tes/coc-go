24d06cfba635ad54f258b292ad781746a84850af
Jackie Li <jackie.li@target-energysolutions.com>
2022-03-02 11:59:03 +0000

    add back gopls commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,54 @@
-import { ExtensionContext, LanguageClient, LanguageClientOptions, commands, services, window, workspace } from 'coc.nvim'
-import { ChildProcess, spawn } from 'child_process'
-import os from 'os'
-import { commandExists, goBinPath, installGoBin } from './utils/tools'
-import { checkGopls, installGomodifytags, installGoplay, installGopls, installGotests, installImpl, installTools, version } from './commands'
-import { addTags, clearTags, removeTags } from './utils/modify-tags'
-import { getConfig, setStoragePath } from './utils/config'
-import { activeTextDocument } from './editor'
-import { GOPLS } from './binaries'
-import { generateTestsAll, generateTestsExported, generateTestsFunction, toogleTests } from './utils/tests'
-import { openPlayground } from './utils/playground'
-import { generateImplStubs } from './utils/impl'
-import { goplsTidy } from './utils/lspcommands'
+import { ChildProcess, spawn } from "child_process"
+import {
+  ExtensionContext,
+  LanguageClient,
+  LanguageClientOptions,
+  commands,
+  listManager,
+  services,
+  window,
+  workspace,
+} from "coc.nvim"
+import os from "os"
+import { GOPLS } from "./binaries"
+import {
+  checkGopls,
+  installGomodifytags,
+  installGoplay,
+  installGopls,
+  installGotests,
+  installImpl,
+  installTools,
+  version,
+} from "./commands"
+import { activeTextDocument } from "./editor"
+import { getConfig, setStoragePath } from "./utils/config"
+import { generateImplStubs } from "./utils/impl"
+import {
+  GoKnownPackagesList,
+  GoTestsList,
+  goplsListKnownPackages,
+  goplsRunTests,
+  goplsTidy,
+} from "./utils/lspcommands"
+import { addTags, clearTags, removeTags } from "./utils/modify-tags"
+import { openPlayground } from "./utils/playground"
+import {
+  generateTestsAll,
+  generateTestsExported,
+  generateTestsFunction,
+  toogleTests,
+} from "./utils/tests"
+import { commandExists, goBinPath, installGoBin } from "./utils/tools"
 
 const restartConfigs = [
-  'go.goplsArgs',
-  'go.goplsOptions',
-  'go.goplsPath',
-  'go.goplsUseDaemon',
+  "go.goplsArgs",
+  "go.goplsOptions",
+  "go.goplsPath",
+  "go.goplsUseDaemon",
 ]
 
 export async function activate(context: ExtensionContext): Promise<void> {
-
   setStoragePath(context.storagePath)
 
   if (getConfig().enable === false) {
@@ -40,10 +68,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
 async function registerGeneral(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.version",
-      () => version()
-    ),
+    commands.registerCommand("go.version", () => version())
   )
 }
 
@@ -57,28 +82,33 @@ async function registerGopls(context: ExtensionContext): Promise<void> {
 
   const args = config.goplsArgs ? [...config.goplsArgs] : []
 
-  if (config.goplsUseDaemon !== false && !args.find(arg => arg.startsWith('-remote'))) {
+  if (
+    config.goplsUseDaemon !== false &&
+    !args.find((arg) => arg.startsWith("-remote"))
+  ) {
     // Use daemon by default
-    args.push('-remote=auto')
+    args.push("-remote=auto")
   }
 
   // TMPDIR needs to be resetted, because its altered by coc.nvim, which breaks
   // the automatic deamon launching of gopls.
   // See: https://github.com/neoclide/coc.nvim/commit/bdd9a9e1401fe6fdd57a9bd078e3651ecf1e0202
-  const tmpdir = await workspace.nvim.eval('$TMPDIR') as string
+  const tmpdir = (await workspace.nvim.eval("$TMPDIR")) as string
 
   const server = (): Promise<ChildProcess> => {
-    return new Promise(resolve => {
-      resolve(spawn(command, args, {
-        cwd: workspace.cwd,
-        env: { ...process.env, TMPDIR: tmpdir, ...config.goplsEnv },
-      }))
+    return new Promise((resolve) => {
+      resolve(
+        spawn(command, args, {
+          cwd: workspace.cwd,
+          env: { ...process.env, TMPDIR: tmpdir, ...config.goplsEnv },
+        })
+      )
     })
   }
 
   // https://github.com/neoclide/coc.nvim/blob/master/src/language-client/client.ts#L684
   const clientOptions: LanguageClientOptions = {
-    documentSelector: ['go', 'gomod'],
+    documentSelector: ["go", "gomod"],
     initializationOptions: () => getConfig().goplsOptions,
     disableWorkspaceFolders: config.disable.workspaceFolders,
     disableDiagnostics: config.disable.diagnostics,
@@ -86,9 +116,9 @@ async function registerGopls(context: ExtensionContext): Promise<void> {
     // TODO disableSnippetCompletion: config.disable.snippetCompletion,
   }
 
-  const client = new LanguageClient('go', 'gopls', server, clientOptions)
+  const client = new LanguageClient("go", "gopls", server, clientOptions)
 
-  if (config.checkForUpdates !== 'disabled' && !config.goplsPath) {
+  if (config.checkForUpdates !== "disabled" && !config.goplsPath) {
     await checkGopls(client, config.checkForUpdates)
   }
 
@@ -97,33 +127,33 @@ async function registerGopls(context: ExtensionContext): Promise<void> {
 
     // restart gopls if options changed
     workspace.onDidChangeConfiguration(async (e) => {
-      if (restartConfigs.find(k => e.affectsConfiguration(k))) {
+      if (restartConfigs.find((k) => e.affectsConfiguration(k))) {
         await client.stop()
         client.restart()
       }
     }),
 
-    commands.registerCommand(
-      "go.install.gopls",
-      () => installGopls(client)
-    )
+    commands.registerCommand("go.install.gopls", () => installGopls(client))
   )
 }
 
 async function goplsPath(goplsPath: string): Promise<string | null> {
   if (goplsPath) {
-    if (goplsPath.startsWith('~')) {
+    if (goplsPath.startsWith("~")) {
       goplsPath = os.homedir() + goplsPath.slice(1)
     }
-    if (!await commandExists(goplsPath)) {
-      window.showMessage(`goplsPath is configured ("${goplsPath}"), but does not exist!`, 'error')
+    if (!(await commandExists(goplsPath))) {
+      window.showMessage(
+        `goplsPath is configured ("${goplsPath}"), but does not exist!`,
+        "error"
+      )
       return null
     }
 
     return goplsPath
   }
 
-  if (!await installGoBin(GOPLS)) {
+  if (!(await installGoBin(GOPLS))) {
     return
   }
 
@@ -132,111 +162,87 @@ async function goplsPath(goplsPath: string): Promise<string | null> {
 
 async function registerGoImpl(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.install.impl",
-      () => installImpl()
-    ),
-    commands.registerCommand(
-      "go.impl.cursor",
-      async () => generateImplStubs(await activeTextDocument())
+    commands.registerCommand("go.install.impl", () => installImpl()),
+    commands.registerCommand("go.impl.cursor", async () =>
+      generateImplStubs(await activeTextDocument())
     )
   )
-
 }
 
 async function registerTest(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.install.gotests",
-      () => installGotests()
+    commands.registerCommand("go.install.gotests", () => installGotests()),
+    commands.registerCommand("go.test.generate.file", async () =>
+      generateTestsAll(await activeTextDocument())
     ),
-    commands.registerCommand(
-      "go.test.generate.file",
-      async () => generateTestsAll(await activeTextDocument())
+    commands.registerCommand("go.test.generate.exported", async () =>
+      generateTestsExported(await activeTextDocument())
     ),
-    commands.registerCommand(
-      "go.test.generate.exported",
-      async () => generateTestsExported(await activeTextDocument())
+    commands.registerCommand("go.test.generate.function", async () =>
+      generateTestsFunction(await activeTextDocument())
     ),
-    commands.registerCommand(
-      "go.test.generate.function",
-      async () => generateTestsFunction(await activeTextDocument())
-    ),
-    commands.registerCommand(
-      "go.test.toggle",
-      async () => toogleTests(await activeTextDocument())
-    ),
+    commands.registerCommand("go.test.toggle", async () =>
+      toogleTests(await activeTextDocument())
+    )
   )
 }
 
 async function registerTags(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.install.gomodifytags",
-      () => installGomodifytags()
+    commands.registerCommand("go.install.gomodifytags", () =>
+      installGomodifytags()
     ),
-    commands.registerCommand(
-      "go.tags.add",
-      async (...tags) => addTags(await activeTextDocument(), { tags })
+    commands.registerCommand("go.tags.add", async (...tags) =>
+      addTags(await activeTextDocument(), { tags })
     ),
-    commands.registerCommand(
-      "go.tags.add.line",
-      async (...tags) => addTags(await activeTextDocument(), { tags, selection: "line" })
+    commands.registerCommand("go.tags.add.line", async (...tags) =>
+      addTags(await activeTextDocument(), { tags, selection: "line" })
     ),
-    commands.registerCommand(
-      "go.tags.add.prompt",
-      async () => addTags(await activeTextDocument(), { prompt: true })
+    commands.registerCommand("go.tags.add.prompt", async () =>
+      addTags(await activeTextDocument(), { prompt: true })
     ),
-    commands.registerCommand(
-      "go.tags.remove",
-      async (...tags) => removeTags(await activeTextDocument(), { tags })
+    commands.registerCommand("go.tags.remove", async (...tags) =>
+      removeTags(await activeTextDocument(), { tags })
     ),
-    commands.registerCommand(
-      "go.tags.remove.line",
-      async (...tags) => removeTags(await activeTextDocument(), { tags, selection: "line" })
+    commands.registerCommand("go.tags.remove.line", async (...tags) =>
+      removeTags(await activeTextDocument(), { tags, selection: "line" })
     ),
-    commands.registerCommand(
-      "go.tags.remove.prompt",
-      async () => removeTags(await activeTextDocument(), { prompt: true })
+    commands.registerCommand("go.tags.remove.prompt", async () =>
+      removeTags(await activeTextDocument(), { prompt: true })
     ),
-    commands.registerCommand(
-      "go.tags.clear",
-      async () => clearTags(await activeTextDocument())
+    commands.registerCommand("go.tags.clear", async () =>
+      clearTags(await activeTextDocument())
     ),
-    commands.registerCommand(
-      "go.tags.clear.line",
-      async () => clearTags(await activeTextDocument(), { selection: "line" })
-    ),
+    commands.registerCommand("go.tags.clear.line", async () =>
+      clearTags(await activeTextDocument(), { selection: "line" })
+    )
   )
 }
 
 async function registerPlaygroud(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.install.goplay",
-      () => installGoplay()
-    ),
-    commands.registerCommand(
-      "go.playground",
-      async () => openPlayground(await activeTextDocument())
+    commands.registerCommand("go.install.goplay", () => installGoplay()),
+    commands.registerCommand("go.playground", async () =>
+      openPlayground(await activeTextDocument())
     )
   )
 }
 
 async function registerTools(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
-    commands.registerCommand(
-      "go.install.tools",
-      () => installTools()
-    )
+    commands.registerCommand("go.install.tools", () => installTools())
   )
 }
 
 async function registerLspCommands(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
+    commands.registerCommand("go.gopls.tidy", goplsTidy),
+    commands.registerCommand("go.gopls.runTests", goplsRunTests),
     commands.registerCommand(
-      "go.gopls.tidy",
-      goplsTidy,
+      "go.gopls.listKnownPackages",
+      goplsListKnownPackages
     ),
+    listManager.registerList(new GoTestsList()),
+    listManager.registerList(new GoKnownPackagesList())
   )
 }
